1382bbf70eee6315f259f1a8d3e78e10429bf6bc
weber1158 <133900499+weber1158@users.noreply.github.com>
2023-05-23 10:04:23 -0400

    Changed rgb colors to hex codes; added ammonium and black carbon plots

--- icePlot.m
+++ icePlot.m
@@ -12,15 +12,20 @@ Acceptable third input arguments:
 =================================
 | PARAMETER | STRING | ASSUMED UNITS |
 |===========|========|===============|
-| oxygen	|		 |               |
-| stable	| 'd18O' |   permil (‰)  |
-| isotopes	|        |               |
+| oxygen    |		 |               |
+| stable    | 'd18O' |   permil (‰)  |
+| isotopes  |        |               |
 |-----------|--------|===============|
-| deuterium	| 'dxs'  |   permil (‰)  |
+| deuterium | 'dxs'  |   permil (‰)  |
 | excess    |		 |               |
 |-----------|--------|===============|
 | nitrate   | 'no3'  |      ppb      |
 |-----------|--------|===============|
+| ammonium  | 'nh4'  |      ppb      |
+|-----------|--------|---------------|
+| black     | 'bc'   |     pg/g      |
+| carbon    |        |               |
+|-----------|--------|---------------|
 | sulfate   | 'so4'  |      ppb      |
 |-----------|--------|===============|
 | chloride  | 'Cl'   |      ppb      |
@@ -28,8 +33,8 @@ Acceptable third input arguments:
 | total     | 'dust' |   counts/mL   |
 | dust      |        |               |
 |-----------|--------|===============|
-| accumula-	| 'acc'  |   m ice eq.   |
-| ation		|		 |               |
+| accumula- | 'acc'  |   m ice eq.   |
+| ation     |        |               |
 |-----------|--------|===============|
 ======================================
 NOTE:
@@ -37,6 +42,10 @@ The units that are used to label the y-axis are
 a default assumption based on the parameter
 chosen. Check the original metadata for the data
 if you are unsure if the default units are correct.
+If so, you can easily modify the y-axis label by
+calling the ylabel() function. For example:
+>> icePlot(xData,yData,'NO3');
+>> ylabel('ppm') % Change ylabel from 'ppb' to 'ppm'
 ======================================
 %}
 
@@ -44,9 +53,12 @@ if you are unsure if the default units are correct.
 % BEGIN CODE
 %%%%%%%%%%%%%
 
+% Font name for plotting
 myfont = 'Lucida Sans Typewriter';
+% Line width for plotting
 lw = 1;
 
+% If no third input argument is given:
 if nargin == 2
 	fig = plot(x,y,'LineWidth',lw);
 	set(gca,...
@@ -55,10 +67,11 @@ if nargin == 2
 		'FontName',myfont)
 	box off
 
+% If third input argument is specified:
 %%%%%%%%%%%%%%%%%%%%%%%%%
 % Oxygen stable isotopes
 %%%%%%%%%%%%%%%%%%%%%%%%%
-elseif parameter_as_string == "d18O"
+elseif parameter_as_string == "d18O" || parameter_as_string == "d18o"
 	fig = plot(x,y,'LineWidth',1);
 	set(gca,...
 		'YGrid','on',...
@@ -66,7 +79,7 @@ elseif parameter_as_string == "d18O"
 		'FontName',myfont)
 	box off
 	ylabel('‰')
-	fig.Color=rgb('blue');
+	fig.Color='#0343df'; % Blue
 	L=legend('δ^{18}O','Location','eastoutside');
 	L.Title.String="Legend";
 
@@ -81,7 +94,7 @@ elseif parameter_as_string == "dxs"
 		'FontName',myfont)
 	box off
 	ylabel('‰')
-	fig.Color=rgb('dark blue');
+	fig.Color='#00035b'; % Dark blue
 	L=legend('{\itd}-excess','Location','eastoutside');
 	L.Title.String="Legend";
 	
@@ -96,8 +109,38 @@ elseif parameter_as_string == "no3" || parameter_as_string == "NO3"
 		'FontName',myfont)
 	box off
 	ylabel('ppb')
-	fig.Color=rgb('green');
+	fig.Color='#15b01a'; % Green
 	L=legend('NO_3^-','Location','eastoutside');
+	L.Title.String="Legend";
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+% Ammonium (NH4+)
+%%%%%%%%%%%%%%%%%%%%%%%%%
+elseif parameter_as_string == "nh4" || parameter_as_string == "NH4"
+	fig = plot(x,y,'LineWidth',lw);
+	set(gca,...
+		'YGrid','on',...
+		'TickDir','out',...
+		'FontName',myfont)
+	box off
+	ylabel('ppb')
+	fig.Color='#6e750e'; % Olive green
+	L=legend('NH_4^+','Location','eastoutside');
+	L.Title.String="Legend";
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+% Black Carbon 
+%%%%%%%%%%%%%%%%%%%%%%%%%
+elseif parameter_as_string == "blackcarbon" || parameter_as_string == "bc" || "BC"
+	fig = plot(x,y,'LineWidth',lw);
+	set(gca,...
+		'YGrid','on',...
+		'TickDir','out',...
+		'FontName',myfont)
+	box off
+	ylabel('ppb')
+	fig.Color='#000000'; % Black
+	L=legend('NH_4^+','Location','eastoutside');
 	L.Title.String="Legend";
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
@@ -111,7 +154,7 @@ elseif parameter_as_string == "so4" || parameter_as_string == "SO4"
 		'FontName',myfont)
 	box off
 	ylabel('ppb')
-	fig.Color=rgb('periwinkle');
+	fig.Color='#8e82fe'; % Periwinkle
 	L=legend('SO_4^{2-}','Location','eastoutside');
 	L.Title.String="Legend";
 
@@ -126,7 +169,7 @@ elseif parameter_as_string == "Cl" || parameter_as_string == "chloride" || param
 		'FontName',myfont)
 	box off
 	ylabel('ppb')
-	fig.Color=rgb('grey');
+	fig.Color='#929591'; % Gr(e/a)y
 	L=legend('Cl^-','Location','eastoutside');
 	L.Title.String="Legend";
 
@@ -141,7 +184,7 @@ elseif parameter_as_string == "dust"
 		'FontName',myfont)
 	box off
 	ylabel('Counts \cdot mL^{-1}')
-	fig.Color=rgb('brown');
+	fig.Color='#653700'; % Brown
 	L=legend('Dust','Location','eastoutside');
 	L.Title.String="Legend";
 
@@ -156,7 +199,7 @@ elseif parameter_as_string == "accumulation" || parameter_as_string == "acc"
 		'FontName',myfont)
 	box off
 	ylabel('m i.e.')
-	fig.Color=rgb('rose');
+	fig.Color='#cf6275'; % Rose pink
 	L=legend('Accumulation','Location','eastoutside');
 	L.Title.String="Legend";
 
